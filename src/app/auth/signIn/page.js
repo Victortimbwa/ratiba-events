0cd6fe2deca96de6b68b26ab81ad66f19c963fd0
Victortimbwa <victortimbwa@gmail.com>
2024-11-06 23:30:41 +0300

    Updates

--- src/app/auth/signIn/page.js
+++ src/app/auth/signIn/page.js
@@ -1,12 +1,16 @@
 "use client";
 import styles from "./signin.module.css";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const SignIn = () => {
 	const router = useRouter();
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
+		// Perform any necessary validation or API calls
 		router.push("/dashboard");
 	};
 
@@ -14,31 +18,33 @@ const SignIn = () => {
 		<section className={styles.homelogin}>
 			<div className={styles.left}>
 				<h1>
-					Welcome to <br></br>
+					Welcome to <br />
 					<span>Ratiba Events</span>
 				</h1>
 			</div>
 			<div className={styles.right}>
 				<h3>LOGIN</h3>
 				<p>Sign in to continue.</p>
-				<form className={styles.loginform}>
+				<form className={styles.loginform} onSubmit={handleSubmit}>
 					<div className={styles.inputbox}>
 						<input
 							type="text"
 							placeholder="Enter Email"
 							name="email"
-							onChange={(e) => setPassword(e.target.value)}
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
 						/>
 					</div>
-					<div className="inputbox">
+					<div className={styles.inputbox}>
 						<input
 							type="password"
 							placeholder="Password"
 							name="password"
+							value={password}
 							onChange={(e) => setPassword(e.target.value)}
 						/>
 					</div>
-					<button type="submit" className="login-button" onClick={handleSubmit}>
+					<button type="submit" className="login-button">
 						Login
 					</button>
 					<p>
